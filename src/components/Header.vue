979ee1125c04fd8d8b86b3fff1ae37e5523d84f3
mcspe <mcspezzaferro@gmail.com>
2023-06-26 17:40:28 +0200

    [add] milestone 4 log results from api call

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<template>
+  <header>
+    <h1>header</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,31 @@
+<script>
+  import axios from 'axios';
+  import {store} from '../store'
+  export default {
+    name: 'Main',
+    data() {
+      return {
+        store
+      }
+    },
+    methods: {
+      apiCall() {
+        axios.get(store.apiEndPoint).then(results => (
+          console.log(results)
+        ));
+      }
+    },
+    mounted() {
+      this.apiCall();    }
+  }
+</script>
+
+<template>
+  <main>
+    <h1>HOME</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+   
+</style>
